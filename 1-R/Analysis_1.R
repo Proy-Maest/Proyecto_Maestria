b057b72cc565ea283cb442eea0efbb0b020758d3
danirorm <dromanr@sura.com.co>
2019-10-24 14:19:10 -0500

    Update Analysis_1.R
    Desarrollo

--- 1-R/Analysis_1.R
+++ 1-R/Analysis_1.R
@@ -1,3 +1,13 @@
 #                           Maestria en ciencia de los Datos
 #                               Proyecto Integrador
 #                              Coeficiente de fidelización al medio de pago tarjeta crédito
+#directorio de trabajo
+
+setwd("~/GitHub/Proyecto_Maestria/2-Data")
+
+#importar data
+library(readr)
+library(dplyr)
+
+data_full<- read_delim("Base_modelo.csv",
+                         ";", escape_double = FALSE, trim_ws = TRUE)
